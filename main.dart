70e678709dd73eb5a97364461dc429821a19a704
arsh0001 <55991119+arsh0001@users.noreply.github.com>
2020-05-23 09:33:47 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playsound (int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+  Expanded buildKey({Color color, int soundNumber}){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){
+          return playsound(soundNumber);
+        },
+        color: color,
+      ),
+    );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Card(
+                child: Center(
+                  child: Text(
+                    'Arsh\'s Xylophone'),
+                ),
+              ),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
